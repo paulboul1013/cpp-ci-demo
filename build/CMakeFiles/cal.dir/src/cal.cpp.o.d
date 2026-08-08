762c1b05270f1e4607b09f1baa1b265c77508168
paulboul1013 <paulboul1013@gamil.com>
2026-08-08 17:00:47 +0800

    remove CMake build files from repository

--- build/CMakeFiles/cal.dir/src/cal.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cal.dir/src/cal.cpp.o: \
- /home/paulboul1013/cpp-ci-demo/src/cal.cpp /usr/include/stdc-predef.h \
- /home/paulboul1013/cpp-ci-demo/include/cal.h
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/paulboul1013/cpp-ci-demo
-# Build directory: /home/paulboul1013/cpp-ci-demo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[cal_test]=] "/home/paulboul1013/cpp-ci-demo/build/test_cal")
-set_tests_properties([=[cal_test]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/paulboul1013/cpp-ci-demo/CMakeLists.txt;39;add_test;/home/paulboul1013/cpp-ci-demo/CMakeLists.txt;0;")
